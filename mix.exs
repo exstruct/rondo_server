a3d773ff36ebbd08e99c4698d88631dd7d4e9ecd
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-09-12 16:01:06 -0600

    release 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule RondoServer.Mixfile do
     [app: :rondo_server,
      version: "0.1.0",
      elixir: "~> 1.2",
+     description: "Usir server for rondo applications",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      consolidate_protocols: Mix.env != :dev,
@@ -13,7 +14,8 @@ defmodule RondoServer.Mixfile do
        "coveralls": :test,
        "coveralls.detai": :test,
        "coveralls.html": :test
-    ],
+     ],
+     package: package,
      deps: deps()]
   end
 
@@ -29,6 +31,14 @@ defmodule RondoServer.Mixfile do
      {:mix_test_watch, "~> 0.2", only: :dev},
      {:cowboy, github: "ninenines/cowboy", only: [:dev, :test]},
      {:excoveralls, "~> 0.5", only: [:dev, :test]},
-     {:websocket_client, github: "jeremyong/websocket_client", only: [:dev, :test]},]
+     {:websocket_client, github: "jeremyong/websocket_client", only: [:dev, :test]},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Cameron Bytheway"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/extruct/rondo_server"}]
   end
 end
